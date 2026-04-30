89a1c9316f38b78e41a03c610b7ee4371d314180
sthernandezci-Fractal <sthernandezci@unal.edu.co>
2026-04-30 14:54:00 -0500

    Incomplete 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,13 +4,21 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-
+import csv
+from pathlib import Path
 
 def pregunta_01():
-    """
-    Retorne la suma de la segunda columna.
+    total=0
+    ruta_csv = Path("files/input/data.csv")
+    with ruta_csv.open(mode='r', encoding='utf-8',newline='') as archivo:
+        lector =csv.reader(archivo, delimiter=';')
+        
+        for fila in lector:
+            try:
+                valor = int(fila[1].strip())
+                total += valor
+            except ValueError:
+                continue
+    print (f"\nTotal Final: {total}")
+    return total
 
-    Rta/
-    214
-
-    """
