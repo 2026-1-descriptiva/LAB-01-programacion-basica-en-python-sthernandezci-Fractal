4bdc9d87fc27a29d79d9222a41e1db45fefb8f02
sthernandezci-Fractal <sthernandezci@unal.edu.co>
2026-05-02 20:20:43 -0500

    Incompleto

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,20 +5,14 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 import csv
-from pathlib import Path
 
 def pregunta_01():
     total=0
-    ruta_csv = Path("files/input/data.csv")
-    with ruta_csv.open(mode='r', encoding='utf-8',newline='') as archivo:
-        lector =csv.reader(archivo, delimiter=';')
+    ruta="files/input/data.csv"
+    with open(ruta, mode='r', encoding='utf-8', newline='') as archivo:
+        lector =csv.reader(archivo,delimiter='\t')
         
         for fila in lector:
-            try:
-                valor = int(fila[1].strip())
-                total += valor
-            except ValueError:
-                continue
-    print (f"\nTotal Final: {total}")
+                total += int(fila[1].strip())
     return total
-
+print ("El total de la columna 2 es:", pregunta_01())
